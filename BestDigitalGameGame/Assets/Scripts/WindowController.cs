9cab83c044c311c138279bb2eb90250c8633725c
Sseptimus <69606116+Sseptimus@users.noreply.github.com>
2024-10-02 11:32:31 +1300

    Clamped Windows to Screen

--- BestDigitalGameGame/Assets/Scripts/WindowController.cs
+++ BestDigitalGameGame/Assets/Scripts/WindowController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
 using UnityEngine.UIElements;
 
 public class WindowController : MonoBehaviour
@@ -11,6 +12,10 @@ public class WindowController : MonoBehaviour
     private Vector2 WorldUnitsInCamera;
     private Vector2 WorldToPixelAmount;
     public Camera Camera;
+    public GameObject ComputerScreen;
+    public GameObject Background;
+    public SpriteRenderer Content;
+    public SpriteRenderer TitleBar;
     void Awake ()
     {
         //Finding Pixel To World Unit Conversion Based On Orthographic Size Of Camera
@@ -36,10 +41,10 @@ public class WindowController : MonoBehaviour
         if (m_bHeld)
         {
             Vector3 moveVec;
-            moveVec.x = transform.position.x +
-                        (ConvertToWorldUnitsX(Input.mousePosition.x) - ConvertToWorldUnitsX(m_vec3MousePos.x));
-            moveVec.y = transform.position.y + (ConvertToWorldUnitsY(Input.mousePosition.y) - ConvertToWorldUnitsY(m_vec3MousePos.y));
+            moveVec.x = Mathf.Clamp(transform.position.x + (ConvertToWorldUnitsX(Input.mousePosition.x) - ConvertToWorldUnitsX(m_vec3MousePos.x)),ComputerScreen.transform.position.x,ComputerScreen.transform.position.x + Background.GetComponent<SpriteRenderer>().bounds.size.x-TitleBar.bounds.size.x);
+            moveVec.y = Mathf.Clamp(transform.position.y + (ConvertToWorldUnitsY(Input.mousePosition.y) - ConvertToWorldUnitsY(m_vec3MousePos.y)),ComputerScreen.transform.position.y - Background.GetComponent<SpriteRenderer>().bounds.size.y+Content.bounds.size.y+TitleBar.bounds.size.y,ComputerScreen.transform.position.y);
             moveVec.z = 0;
+            
             transform.SetPositionAndRotation(moveVec,Quaternion.identity);
             m_vec3MousePos = Input.mousePosition;
         }
