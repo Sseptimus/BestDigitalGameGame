1ccc561da4a0bcfdcb716ab515a5fe4a3e1f61f5
CharliSIO <c.jones89623@gmail.com>
2024-10-11 17:16:55 +1300

    Trying to get squares to not spawn on top of each other.

--- BestDigitalGameGame/Assets/Scripts/MinigameScripts/ChimpsGame.cs
+++ BestDigitalGameGame/Assets/Scripts/MinigameScripts/ChimpsGame.cs
@@ -23,7 +23,25 @@ public class ChimpsGame : MonoBehaviour
         arrChimpSquares = new GameObject[arrChimpSquaresToLoad.Length];
        for (int i = 0; i < arrChimpSquaresToLoad.Length; i++)
         {
-            arrChimpSquaresToLoad[i] = Instantiate(arrChimpSquaresToLoad[i], new Vector3((int)Random.Range(GameWindow.transform.position.x, GameWindowContent.size.x + GameWindow.transform.position.x), (int)Random.Range(GameWindow.transform.position.y, GameWindowContent.size.y + GameWindow.transform.position.y), -2), Quaternion.identity) as GameObject;
+            arrChimpSquaresToLoad[i] = Instantiate(arrChimpSquaresToLoad[i], 
+                new Vector3((int)Random.Range(GameWindowContent.transform.position.x, GameWindowContent.size.x + GameWindowContent.transform.position.x), 
+                            (int)Random.Range(GameWindowContent.transform.position.y, GameWindowContent.size.y + GameWindowContent.transform.position.y), -2), 
+                Quaternion.identity, GameWindowContent.transform) as GameObject;
+            for (int j = 0; j < i; j++)
+            {
+                bool placedCorrectly = false;
+                while (!placedCorrectly)
+                {
+                    if (arrChimpSquaresToLoad[i].GetComponent<BoxCollider2D>().transform.position.x == arrChimpSquaresToLoad[j].GetComponent<BoxCollider2D>().transform.position.x
+                        && arrChimpSquaresToLoad[i].GetComponent<BoxCollider2D>().transform.position.y == arrChimpSquaresToLoad[j].GetComponent<BoxCollider2D>().transform.position.y)
+                    {
+                        arrChimpSquaresToLoad[i].transform.position = new Vector3((int)Random.Range(GameWindowContent.transform.position.x, GameWindowContent.size.x + GameWindowContent.transform.position.x),
+                            (int)Random.Range(GameWindowContent.transform.position.y, GameWindowContent.size.y + GameWindowContent.transform.position.y), -2);
+                    }
+                    else {
+                        placedCorrectly = true; break; }
+                }
+            }
         }
     }
 
