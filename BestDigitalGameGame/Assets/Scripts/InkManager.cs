6d844af8ac0ab0a6058ea142e0010f0c257d3d41
Sseptimus <69606116+Sseptimus@users.noreply.github.com>
2024-10-30 15:03:08 +1300

    Chat is working properly
    Text can still be too small

--- BestDigitalGameGame/Assets/Scripts/InkManager.cs
+++ BestDigitalGameGame/Assets/Scripts/InkManager.cs
@@ -16,7 +16,7 @@ public class InkManager : MonoBehaviour
     private Story _story;
     
     [SerializeField]
-    private VerticalLayoutGroup _choiceButtonContainer;
+    private HorizontalLayoutGroup _choiceButtonContainer;
 
     [SerializeField]
     private Button _choiceButtonPrefab;
@@ -47,7 +47,7 @@ public class InkManager : MonoBehaviour
         if (iAlternate % 3 == 0 && sQueuedText.Length != 0)
         {
             sOutputText += sQueuedText[0];
-            sQueuedText = sQueuedText.TrimStart(sQueuedText[0]);
+            sQueuedText = sQueuedText.Remove(0, 1);
         }
         if (ChatController.CurrentMessage)
         {
@@ -55,6 +55,10 @@ public class InkManager : MonoBehaviour
         }
         
         iAlternate++;
+        if (sQueuedText.Length == 0)
+        {
+            DisplayNextLine();
+        }
     }
 
     private void StartStory()
@@ -79,20 +83,24 @@ public class InkManager : MonoBehaviour
         }
     }
 
-    private void PrintToScreen(string newText)
+    private void PrintToScreen(string newText,bool _bIsPlayer = false)
     {
         sQueuedText = newText;
         sOutputText = "";
-        if (ChatController.NPCMessages.Count == 0||ChatController.NPCMessages[ChatController.NPCMessages.Count - 1] ==
-            ChatController.CurrentMessage)
+        if (_bIsPlayer)
         {
             ChatController.CurrentMessage = Instantiate(ChatController.MessagePrefab, ChatController.PlayerMessageContainer.transform, false);
             ChatController.PlayerMessages.Append(ChatController.CurrentMessage);
+            ChatController.CurrentMessage.text = newText;
+            ChatController.NPCMessages.Append(Instantiate(ChatController.MessagePrefab, ChatController.NPCMessageContainer.transform, false));
+            ChatController.CurrentMessage.alignment = TextAlignmentOptions.Right;
         }
         else
         {
             ChatController.CurrentMessage = Instantiate(ChatController.MessagePrefab, ChatController.NPCMessageContainer.transform, false);
             ChatController.NPCMessages.Append(ChatController.CurrentMessage);
+            ChatController.PlayerMessages.Append(Instantiate(ChatController.MessagePrefab, ChatController.PlayerMessageContainer.transform, false));
+            ChatController.CurrentMessage.alignment = TextAlignmentOptions.Left;
         }
 
     }
@@ -124,6 +132,7 @@ public class InkManager : MonoBehaviour
     }
     void OnClickChoiceButton(Choice choice)
     {
+        PrintToScreen(choice.text,true);
         _story.ChooseChoiceIndex(choice.index); // tells ink which choice was selected
         RefreshChoiceView(); // removes choices from the screen
         DisplayNextLine();
